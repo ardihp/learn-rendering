9b14f4b90dad2a969a3ed29360dfdbb262f332be
ardihp <ardihp8@gmail.com>
2024-05-02 18:01:22 +0700

    feat: implement daisyjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        bug: "#93bb37",
+        poison: "#a663c3",
+        normal: "#929aa2",
+        fighting: "#d03e68",
+        flying: "#94aee1",
+        ground: "#d97b47",
+        rock: "#c9ba8d",
+        ghost: "#556ab3",
+        steel: "#528b9e",
+        fire: "#ff9f52",
+        water: "#58a3dc",
+        grass: "#5fbc5a",
+        electric: "#f5d646",
+        psychic: "#f9797d",
+        ice: "#7cd3c7",
+        dragon: "#026fc1",
+        dark: "#5c5569",
+        fairy: "#ee93e5",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        "spin-slow": "spin 7s linear infinite",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 export default config;
