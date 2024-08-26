4fb41a9144817892b4dd373756b44e1fb183cff4
ardihp <ardihp8@gmail.com>
2024-08-26 14:20:18 +0700

    fix: client & server rendering loading page

--- src/app/server-rendering/loading.tsx
+++ src/app/server-rendering/loading.tsx
@@ -1,10 +1,14 @@
-import Loader from "@/section/loading";
+import SkeletonCard from "@/section/client/components/skeleton-card";
 import React from "react";
 
 export default function LoadingPage() {
   return (
-    <div className="flex flex-col gap-4 w-full">
-      <Loader />;
+    <div className="flex flex-col w-full">
+      <div className="grid grid-cols-5 gap-6">
+        {[...new Array(15)]?.map((item, key) => (
+          <SkeletonCard key={key} />
+        ))}
+      </div>
     </div>
   );
 }
