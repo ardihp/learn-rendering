d1d78fdd4def0594b43e6f9b03866a43f88389f8
ardihp <ardihp8@gmail.com>
2024-08-27 13:28:20 +0700

    refactor(client): folder naming

--- src/app/client-rendering/[name]/page.tsx
+++ src/app/client-rendering/[name]/page.tsx
@@ -1,14 +1,16 @@
+"use client";
+
 import React from "react";
 import ClientDetailPokemonView from "@/section/client/detail/view";
 
 interface ClientDetailPokemonProps {
   params: {
-    slug: string;
+    name: string;
   };
 }
 
 export default function ClientDetailPokemon({
   params,
 }: ClientDetailPokemonProps) {
-  return <ClientDetailPokemonView pokemon={params?.slug || ""} />;
+  return <ClientDetailPokemonView pokemon={params?.name || ""} />;
 }
