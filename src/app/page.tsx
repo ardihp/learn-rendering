bcfc9da072ce996079d50d5115b8f9a4944aa752
ardihp <ardihp8@gmail.com>
2024-08-28 11:39:09 +0700

    fix: index content page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
 
         <Link
           href="/server-rendering"
-          className="group rounded-lg flex flex-col items-start border border-transparent px-5 py-4 transition-colors hover:border-neutral-700 hover:bg-neutral-800/30"
+          className="group rounded-lg flex flex-col items-start border border-transparent px-5 py-4 transition-colors border-neutral-700 bg-neutral-800/30"
           passHref
         >
           <h2 className="md:mb-3 text-lg md:text-2xl font-medium">
